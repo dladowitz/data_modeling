c613ca042e634514568bb18ff75849ced65631f4
David Ladowitz <david@ladowitz.com>
2016-08-24 22:31:17 -0700

    fuck you select tag

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,13 +5,18 @@ class OrdersController < ApplicationController
     @orders= Order.all
   end
 
+  def new
+    @order = Order.new
+  end
+
   def create
     order = Order.new(order_params)
     order.shipping_date = 2.days.from_now
     order.save
 
     params[:order][:line_items].each do |line_item|
-      order.line_items.create(line_item.to_hash)
+      # order.line_items.create(line_item.to_hash)
+      order.line_items.create(purchasable_type: "Book", purchasable_id: line_item, quantity: 1, item_price: 1000)
     end
 
     redirect_to orders_path
