d962c627492ca7bad8c51dd8f0c974ffbb5a64c0
David Ladowitz <david@ladowitz.com>
2016-08-24 18:37:41 -0700

    added create specs

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,25 @@
 class OrdersController < ApplicationController
   def index
-
+    @orders= Order.all
   end
+
+  def create
+    order = Order.create(order_params)
+    order.shipping_date = 2.days.from_now
+    order.save
+
+    params[:order][:line_items].each do |line_item|
+      order.line_items.create(line_item.to_hash)
+    end
+
+    redirect_to orders_path
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:shipping_address)
+  end
+
 
 end
--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 describe OrdersController do
+  let(:author) { Author.create(name: FFaker::Name.first_name) }
+  let(:book) { author.books.create(title: FFaker::Skill.tech_skill) }
+  let(:video) { Video.create(name:FFaker::Name.first_name, length: 102) }
+
   describe "#index" do
     subject { get :index }
-    before do
-      author = Author.create(name: FFaker::Name.first_name)
-      book = author.books.create(title: FFaker::Skill.tech_skill)
 
+    before do
       2.times do
          order = Order.create shipping_address: "12345 Main St", shipping_date: 1.week.from_now
          order.line_items.create(purchasable_type: "book", purchasable_id: book.id, item_price: 999, quantity: 2)
@@ -20,6 +22,23 @@ describe OrdersController do
 
     it "finds all the orders in the database" do
       expect(assigns(:orders).count).to eq 2
+    end
+  end
+
+  describe "#create" do
+    subject { post :create,  order: { shipping_address: "1000 Main St", line_items: [{ purchasable_type: "book", purchasable_id: book.id, item_price: 999, quantity: 2 }, { purchasable_type: "video", purchasable_id: video.id, item_price: 1000, quantity: 1 }] }}
+    before { subject }
+
+    it "creates a new order in the database" do
+      expect(Order.first.shipping_address).to eq "1000 Main St"
+    end
+
+    it "creates new line items in the database" do
+      expect(LineItem.count).to eq 2
+    end
+
+    it "associates the line items with the order" do
+      expect(LineItem.first.order.shipping_address).to eq "1000 Main St"
     end
   end
 end
